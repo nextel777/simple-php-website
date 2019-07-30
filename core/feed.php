7d0fe8e6776680b4a2cbc6cb9107ffc50c69c5bb
Igor Spizhovyi <igor.spizhovyi@gmail.com>
2019-07-30 18:00:22 +0300

    continue working with post

--- core/feed.php
+++ core/feed.php
@@ -10,7 +10,7 @@ if (!$_GET['id']){
     while ($posts_array = $result->fetch_array()) {
 
         echo '<div class="post-'.$posts_array['id'].'">
-           <h2>' . $posts_array['title'] .'</h2>
+           <a href="feed.php?id='.$posts_array['id'].'"><h2>' . $posts_array['title'] .'</h2></a>
            <div class="content"><p>'. $posts_array['description'].'</p></div>
            <div class="date">date '. $posts_array['date'].'</div>
            <div class="author">posted by '.$posts_array['username'].'</div>
